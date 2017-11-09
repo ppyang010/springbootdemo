53d34dc2f490584b874bbb13dda5a43221e9fecb
caichengyang <caicy@dxy.cn>
2017-11-09 19:50:09 +0800

    适应新环境

--- springdemo/src/main/java/com/code/SpringdemoApplication.java
+++ springdemo/src/main/java/com/code/SpringdemoApplication.java
@@ -3,6 +3,10 @@ package com.code;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author ccy
+ */
 @SpringBootApplication
 public class SpringdemoApplication {
 
