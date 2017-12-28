b94c80c354fb02a590b4fd17907cc6631d6b5715
Administrator <644724460@qq.com>
2017-12-28 23:22:28 +0800

    new: config

---
+++ springdemo/src/main/java/com/code/auth/config/ShiroCofig.java
@@ -0,0 +1,10 @@
+package com.code.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Administrator on 2017/12/28.
+ */
+@Configuration
+public class ShiroCofig {
+}
---
+++ springdemo/src/main/java/com/code/auth/config/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2017/12/28.
+ * spring配置类包
+ */
+package com.code.auth.config;
