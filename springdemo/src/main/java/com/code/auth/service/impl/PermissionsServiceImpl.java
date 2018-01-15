7207bb166e1cd054c578ecd32afdc7972aef06fe
caichengyang <caicy@dxy.cn>
2018-01-15 21:56:44 +0800

    shiro登录和授权 20180115

--- springdemo/src/main/java/com/code/auth/service/impl/PermissionsServiceImpl.java
+++ springdemo/src/main/java/com/code/auth/service/impl/PermissionsServiceImpl.java
@@ -3,10 +3,16 @@ package com.code.auth.service.impl;
 import com.code.auth.service.PermissionsService;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 /**
  * 权限业务层
  * @author ccy
  */
 @Service
 public class PermissionsServiceImpl implements PermissionsService{
+    @Override
+    public Set<String> listPermissionsByUsername(String username) {
+        return null;
+    }
 }
