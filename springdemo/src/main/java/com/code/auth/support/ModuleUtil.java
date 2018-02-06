25c4a578bf81c72487ebdf1fa387e6b3cb2fac81
caichengyang <caicy@dxy.cn>
2018-02-06 11:40:09 +0800

    20180206

--- springdemo/src/main/java/com/code/auth/support/ModuleUtil.java
+++ springdemo/src/main/java/com/code/auth/support/ModuleUtil.java
@@ -9,7 +9,8 @@ public class ModuleUtil {
     public static int DEFAULT_PAGE_NO = 1;
     public static int DEFAULT_PAGE_SIZE = 10;
     public static int MAX_PAGE_SIZE = 500;
-    static PageRequest toPageable(PageBean pageBean, Sort sort) {
+
+    public static PageRequest toPageable(PageBean pageBean, Sort sort) {
         return new PageRequest(
                 Math.max(0, pageBean.getPageNo() - 1),
                 Math.min(MAX_PAGE_SIZE, pageBean.getPageSize() > 0 ? pageBean.getPageSize() : DEFAULT_PAGE_SIZE),
