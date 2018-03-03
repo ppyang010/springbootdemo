9a20da03688d6984e6150591d0b030f9800cf519
caichengyang <caicy@dxy.cn>
2018-03-04 01:49:20 +0800

    20180304

--- springdemo/src/main/java/com/code/auth/web/controller/LoginController.java
+++ springdemo/src/main/java/com/code/auth/web/controller/LoginController.java
@@ -3,10 +3,15 @@ package com.code.auth.web.controller;
 import com.code.auth.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
     @Autowired
     UserService userService;
+    @RequestMapping("/doLogin")
+    public String doLogin(){
+        return "index";
+    }
 
 }
--- springdemo/src/main/java/com/code/auth/web/controller/RouteController.java
+++ springdemo/src/main/java/com/code/auth/web/controller/RouteController.java
@@ -3,6 +3,7 @@ package com.code.auth.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.RequestContext;
 
 @Controller
 public class RouteController {
@@ -10,5 +11,10 @@ public class RouteController {
     @RequestMapping(value = {"/index","/"})
     public String index(Model model){
         return "index";
+    }
+
+    @RequestMapping("/admin/login")
+    public String login (Model model){
+        return "login";
     }
 }
