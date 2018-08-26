788924efd6dd92288e4db9a46f5a250e7f329db4
ppyang010 <644724460@qq.com>
2018-08-26 23:03:59 +0800

    my start demo

--- mystarter/src/main/java/com/code/MyStartConfig.java
+++ mystarter/src/main/java/com/code/MyStartConfig.java
@@ -3,23 +3,37 @@ package com.code;
 import com.code.domain.HelloProperties;
 import com.code.service.HelloService;
 import com.code.service.HelloServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 
 @EnableConfigurationProperties({HelloProperties.class})
-@ConditionalOnClass
+//@ConditionalOnClass(HelloService.class)//存在HelloService时初始化该配置类
+@ConditionalOnProperty//存在对应配置信息时初始化该配置类
+        (
+                prefix = "hello",//存在配置前缀hello
+                value = "enabled",//开启
+                matchIfMissing = true//缺失检查
+        )
 public class MyStartConfig {
 
+    //application.properties配置文件映射前缀实体对象
+    @Autowired
+    private HelloProperties helloProperties;
+
+    @Bean
     @ConditionalOnMissingBean(HelloService.class)
     public HelloService helloService(){
         System.out.println(">>>The HelloService Not Found，Execute Create New Bean.");
         HelloService helloService = new HelloServiceImpl();
         //设置消息内容
-        helloService.sayHello("test");
+        helloService.sayHello("init" + helloProperties.getMsg());
         return helloService;
     }
 
--- mystarter/src/main/java/com/code/domain/HelloProperties.java
+++ mystarter/src/main/java/com/code/domain/HelloProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "hello")
 public class HelloProperties {
     //消息内容
-    private String msg = "HengYu";
+    private String msg = "default";
     //是否显示消息内容
     private boolean show = true;
 
