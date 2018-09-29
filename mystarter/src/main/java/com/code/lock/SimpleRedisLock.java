bdabae4339e09c044fee0fdf49b29293977623bd
caichengyang <caicy@dxy.cn>
2018-09-29 18:41:15 +0800

    redis lock

---
+++ mystarter/src/main/java/com/code/lock/SimpleRedisLock.java
@@ -0,0 +1,60 @@
+package com.code.lock;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.Collections;
+
+/**
+ * redis lock
+ *
+ * @author ccy
+ */
+@Component
+public class SimpleRedisLock {
+
+    @Autowired
+    private JedisPool jedisPool;
+
+    private static final String SET_IF_NOT_EXIST = "NX";
+    private static final String SET_WITH_EXPIRE_TIME = "PX";
+    private static final String LOCK_PREFIX = "dlock_";
+    private static final String LOCK_MSG = "OK";
+
+    private static final Long UNLOCK_MSG = 1L;
+    private static final int DEFAULT_EXPIRE_TIME = 60 * 1000;
+    private static final long DEFAULT_SLEEP_TIME = 100;
+
+
+    public void lock(String key, String request, int timeout) {
+        Jedis jedis = jedisPool.getResource();
+        try {
+            while (timeout >= 0) {
+                String result = jedis.set(LOCK_PREFIX + key, request, SET_IF_NOT_EXIST, SET_WITH_EXPIRE_TIME, DEFAULT_EXPIRE_TIME);
+                if (LOCK_MSG.equals(result)) {
+                    jedis.close();
+                    return;
+                }
+                Thread.sleep(DEFAULT_SLEEP_TIME);
+                timeout -= DEFAULT_SLEEP_TIME;
+
+            }
+        } catch (InterruptedException e) {
+            jedis.close();
+            e.printStackTrace();
+        }
+    }
+
+
+    public boolean unlock(String key, String value) {
+        Jedis jedis = jedisPool.getResource();
+
+        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
+        Object result = jedis.eval(script, Collections.singletonList(LOCK_PREFIX + key), Collections.singletonList(value));
+
+        jedis.close();
+        return UNLOCK_MSG.equals(result);
+    }
+}
--- springdemo/src/main/java/com/code/hello/HelloController.java
+++ springdemo/src/main/java/com/code/hello/HelloController.java
@@ -1,16 +1,21 @@
 package com.code.hello;
 
+import com.code.lock.SimpleRedisLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by Administrator on 2017/10/3.
  */
 @RestController
 public class HelloController {
+    public static Integer STOCK ;
 
     @Value("${myParam}")
     private String myParam;
@@ -19,6 +24,9 @@ public class HelloController {
     private People people;
     @Autowired
     private JedisPool jedisPool;
+
+    @Autowired
+    private SimpleRedisLock simpleRedisLock;
 
     @RequestMapping(value = {"hello", "hi"}, method = RequestMethod.GET)
     public String hello() {
@@ -39,6 +47,72 @@ public class HelloController {
             return jedis.get("test");
         } finally {
             jedis.close();
+        }
+    }
+
+    @RequestMapping(value = "noLock", method = RequestMethod.GET)
+    public Integer redisLockTest() throws InterruptedException {
+        reset();
+        Thread.sleep(1);
+        final int[] count = {0};
+        for (int i = 0; i < 100; i++) {
+            int finalI = i;
+            new Thread(() -> {
+
+                try {
+                    buyStock(count);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                }
+            },"simpleLockThread-"+finalI).start();
+        }
+        Thread.sleep(5000);
+
+        System.out.println(count[0]);
+        return STOCK;
+    }
+
+    @RequestMapping(value = "reset", method = RequestMethod.GET)
+    public String reset() {
+        STOCK = 10;
+        return STOCK.toString();
+    }
+
+    @RequestMapping(value = "simpleLock", method = RequestMethod.GET)
+    public Integer simpleLock() throws InterruptedException {
+        reset();
+        Thread.sleep(1);
+        final int[] count = {0};
+        for (int i = 0; i < 100; i++) {
+            int finalI = i;
+            new Thread(() -> {
+
+                try {
+                    simpleRedisLock.lock("simpleLock", String.valueOf(finalI), 1000);
+                    buyStock(count);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    simpleRedisLock.unlock("simpleLock", String.valueOf(finalI));
+                }
+            },"simpleLockThread-"+finalI).start();
+        }
+        Thread.sleep(5000);
+
+        System.out.println(count[0]);
+        return STOCK;
+    }
+
+    private void buyStock(int[] count) throws InterruptedException {
+        int s = STOCK;
+        Thread.sleep(1);
+        if (s > 0) {
+            STOCK = s - 1;
+            count[0]++;
+            System.out.println("购买成功");
+        } else {
+            System.out.println("购买失败");
         }
     }
 
