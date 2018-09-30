617f3a0df34e98e3aea270f7ada8043b589c74f6
caichengyang <caicy@dxy.cn>
2018-09-30 18:06:19 +0800

    redis lock

--- mystarter/src/main/java/com/code/lock/SimpleRedisLock.java
+++ mystarter/src/main/java/com/code/lock/SimpleRedisLock.java
@@ -9,7 +9,8 @@ import java.util.Collections;
 
 /**
  * redis lock
- *
+ *  实现参考
+ *  http://www.wangtianyi.top/blog/2018/06/24/shou-ba-shou-jiao-ni-she-ji-bing-shi-xian-yi-ge-ji-yu-redisde-fen-bu-shi-suo/
  * @author ccy
  */
 @Component
@@ -20,11 +21,17 @@ public class SimpleRedisLock {
 
     private static final String SET_IF_NOT_EXIST = "NX";
     private static final String SET_WITH_EXPIRE_TIME = "PX";
-    private static final String LOCK_PREFIX = "dlock_";
+    private static final String LOCK_PREFIX = "SimpleRedisLock_";
     private static final String LOCK_MSG = "OK";
 
     private static final Long UNLOCK_MSG = 1L;
+    /**
+     * 默认超时时间
+     */
     private static final int DEFAULT_EXPIRE_TIME = 60 * 1000;
+    /**
+     * 默认睡眠时间
+     */
     private static final long DEFAULT_SLEEP_TIME = 100;
 
 
@@ -48,11 +55,11 @@ public class SimpleRedisLock {
     }
 
 
-    public boolean unlock(String key, String value) {
+    public boolean unlock(String key, String request) {
         Jedis jedis = jedisPool.getResource();
 
         String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
-        Object result = jedis.eval(script, Collections.singletonList(LOCK_PREFIX + key), Collections.singletonList(value));
+        Object result = jedis.eval(script, Collections.singletonList(LOCK_PREFIX + key), Collections.singletonList(request));
 
         jedis.close();
         return UNLOCK_MSG.equals(result);
