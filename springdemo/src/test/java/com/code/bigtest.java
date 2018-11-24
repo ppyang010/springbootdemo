072fc2340e7c4dd87d6f6676928d5c61055ebfdb
caichengyang <caicy@dxy.cn>
2018-11-24 18:03:25 +0800

    test code

--- springdemo/src/test/java/com/code/bigtest.java
+++ springdemo/src/test/java/com/code/bigtest.java
@@ -1,6 +1,11 @@
 package com.code;
 
 import java.math.BigDecimal;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author ccy
@@ -8,8 +13,20 @@ import java.math.BigDecimal;
  * @time 2018/11/14 下午12:08
  */
 public class bigtest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         BigDecimal bigDecimal = new BigDecimal("1.32").setScale(1, BigDecimal.ROUND_UP);
         System.out.println(bigDecimal.toString());
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+        countDownLatch.await();
+        countDownLatch.countDown();
+        Semaphore semaphore = new Semaphore(10);
+        semaphore.acquire(1);
+        semaphore.release(1);
+        AtomicInteger atomicInteger = new AtomicInteger(10);
+        atomicInteger.incrementAndGet();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.submit(()->{
+
+        });
     }
 }
