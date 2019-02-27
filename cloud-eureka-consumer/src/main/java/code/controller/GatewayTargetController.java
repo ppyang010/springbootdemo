a013ebd93817534123f5e741ef9c923485114241
caichengyang <caicy@dxy.cn>
2019-02-27 18:33:19 +0800

    接入注册中心

--- cloud-eureka-consumer/src/main/java/code/controller/GatewayTargetController.java
+++ cloud-eureka-consumer/src/main/java/code/controller/GatewayTargetController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * gateway 路由跳转的目标地址
@@ -13,7 +16,13 @@ import javax.servlet.http.HttpServletRequest;
 public class GatewayTargetController {
 
     @GetMapping("/gateway/target")
-    public String target(HttpServletRequest request){
+    public String target(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("----request head----");
+        System.out.println(request.getHeader("head"));
+        System.out.println("----request param----");
+        System.out.println(request.getQueryString());
+        System.out.println("----response head----");
+        System.out.println(response.getHeader("respHead"));
         System.out.println(request.getHeader("hello"));
         return "target return";
     }
