0111ba7fb6567346c676e22e64bbee429adbb521
caichengyang <caicy@dxy.cn>
2019-05-05 11:27:14 +0800

    todo 更新

--- springdemo/src/test/java/com/code/bigtest.java
+++ springdemo/src/test/java/com/code/bigtest.java
@@ -1,6 +1,7 @@
 package com.code;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
@@ -9,6 +10,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 /**
  * @author ccy
@@ -17,12 +19,27 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class bigtest {
     public static void main(String[] args) throws InterruptedException {
+//        bigtest();
+        int i = 1;
+        i++;
+//        i = i++;
+//        int j = i++;//1
+//        int k = i + ++i * i++;
+//        System.out.println(i);
+//        System.out.println(j);
+//        System.out.println(k);
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+        countDownLatch.await();
+    }
+
+    public static void bigtest() throws InterruptedException {
         BigDecimal bigDecimal = new BigDecimal("1.32").setScale(1, BigDecimal.ROUND_UP);
         System.out.println(bigDecimal.toString());
         CountDownLatch countDownLatch = new CountDownLatch(3);
         countDownLatch.await();
         countDownLatch.countDown();
         Semaphore semaphore = new Semaphore(10);
+        semaphore.tryAcquire(1000,TimeUnit.MILLISECONDS);
         semaphore.acquire(1);
         semaphore.release(1);
         AtomicInteger atomicInteger = new AtomicInteger(10);
@@ -31,17 +48,21 @@ public class bigtest {
         executorService.submit(()->{
 
         });
+        executorService.execute(()->{
+
+        });
 
         ReentrantLock reentrantLock = new ReentrantLock();
         Condition condition =reentrantLock.newCondition();
 
         reentrantLock.lock();
         reentrantLock.tryLock();
-        reentrantLock.tryLock(1000,TimeUnit.SECONDS);
+        reentrantLock.tryLock(1000, TimeUnit.SECONDS);
         reentrantLock.unlock();
         condition.await();
         condition.signal();
         condition.signalAll();
 
+        ArrayList<Object> list = new ArrayList<>();
     }
 }
--- todo.md
+++ todo.md
@@ -21,7 +21,9 @@
 23 request body 重复读  
 24 retry任务重试机制(异步任务) guava 和spring retry方案   
 25 sentry
-26 字符串模板(beelt,sf4j)
+26 spring 线程池  
+27 多线程遍历集合
+28 字符串模板(beelt,sf4j)
 
 
 ###done
