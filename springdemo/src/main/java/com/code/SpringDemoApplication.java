fbbb4b62184978906bdddfe9aa6332b7464af69a
CCY <644724460@qq.com>
2019-08-27 22:49:16 +0800

    demo

--- springdemo/src/main/java/com/code/SpringDemoApplication.java
+++ springdemo/src/main/java/com/code/SpringDemoApplication.java
@@ -6,9 +6,11 @@ import com.code.auth.config.ShiroConfig;
 import com.code.data.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -25,18 +27,24 @@ public class SpringDemoApplication {
 
     public static void main(String[] args) {
 //        SpringApplication.run(SpringDemoApplication.class, args);
-		SpringApplication springApplication = new SpringApplication(SpringDemoApplication.class);
-//		// 设置为 非 web 应用
-//		springApplication.setWebEnvironment(false);
-		ConfigurableApplicationContext context = springApplication.run(args);
-		//从spring中获取bean
+        SpringApplication springApplication = new SpringApplication(SpringDemoApplication.class);
+        //设置为 非 web 应用
+        //springApplication.setWebEnvironment(false);
+        ConfigurableApplicationContext context = springApplication.run(args);
+        //从spring中获取bean
 //        MyAop bean = context.getBean(MyAop.class);
         //进行处理
 //        bean.findOne(1);
-        System.out.println("end");
+//        System.out.println("end");
 //		System.out.println(context.getBean(SpringDemoApplication.class));
 //		// 输出当前 Spring Boot 应用的 ApplicationContext 的类名
 //		System.out.println("当前 Spring 应用上下文的类：" + context.getClass().getName());
+
+    }
+
+    @EventListener(WebServerInitializedEvent.class)
+    public void onWebServerReady(WebServerInitializedEvent event) {
+        System.out.println("当前webServer实现类为：" + event.getWebServer().getClass().getName());
 
     }
 
