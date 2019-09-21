10fad26059b38e6fe256ebccee1e6da7c9a0ead1
caichengyang <caicy@dxy.cn>
2019-09-21 16:23:41 +0800

    rocket mq console ok

--- demo-recoketmq/docker/mac/console/start.sh
+++ demo-recoketmq/docker/mac/console/start.sh
@@ -1,4 +1,15 @@
 
 # 参考:https://blog.csdn.net/sinat_28434649/article/details/79295164
 
-docker run --link rmq-namesrv:rmqnamesrv  -e "JAVA_OPTS=-Drocketmq.namesrv.addr=192.168.202.119:29876 -Dcom.rocketmq.sendMessageWithVIPChannel=false" -p 8001:8080 -t styletang/rocketmq-console-ng
+docker run --link rmq-namesrv:rmqnamesrv \
+-e "JAVA_OPTS=-Drocketmq.namesrv.addr=192.168.202.119:29876 -Dcom.rocketmq.sendMessageWithVIPChannel=false" \
+-p 8001:8080 -t styletang/rocketmq-console-ng
+
+
+## 后台运行
+docker run --link rmq-namesrv:rmqnamesrv \
+-e "JAVA_OPTS=-Drocketmq.namesrv.addr=192.168.202.119:29876 -Dcom.rocketmq.sendMessageWithVIPChannel=false" \
+-p 8001:8080 -d styletang/rocketmq-console-ng
+
+
+localhost:8001
