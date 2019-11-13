12b9c98f357d281d01e4c397b9571fcef103532a
caichengyang <caicy@dxy.cn>
2019-11-14 00:35:04 +0800

    rocket-mq demo  优化

--- demo-recoketmq/src/main/java/com/code/example/simple/RocketMqUtil.java
+++ demo-recoketmq/src/main/java/com/code/example/simple/RocketMqUtil.java
@@ -13,36 +13,16 @@ public class RocketMqUtil {
     /**
      * mac
      */
-//    public static final String NAMESRV_ADDR = "192.168.202.119:29876";
+    public static final String NAMESRV_ADDR = "192.168.202.119:29876";
     /**
      * win
      */
-    public static final String NAMESRV_ADDR = "192.168.190.209:29876";
+//    public static final String NAMESRV_ADDR = "192.168.190.209:29876";
     /**
      * mac home
      */
 //    public static final String NAMESRV_ADDR = "192.168.190.103:29876";
 
-    public static DefaultMQProducer producer;
-
-    static {
-        producer = new
-                DefaultMQProducer("please_rename_unique_group_name");
-        // Specify name server addresses.
-//        producer.setNamesrvAddr("129.204.121.60:9876");
-        //mac
-        producer.setNamesrvAddr("192.168.202.119:29876");
-        //win
-//        producer.setNamesrvAddr("192.168.190.209:29876");
-        //        mac home
-//        producer.setNamesrvAddr("192.168.190.103:29876");
-        //Launch the instance.
-        try {
-            producer.start();
-        } catch (MQClientException e) {
-            e.printStackTrace();
-        }
-    }
 
     public static DefaultMQProducer getDefaultMQProducer(String  groupname){
         DefaultMQProducer producer = new DefaultMQProducer(groupname);
