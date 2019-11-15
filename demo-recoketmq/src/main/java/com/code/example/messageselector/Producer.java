1d188b48747aea714f090f83d355b78febb5a393
caichengyang <caicy@dxy.cn>
2019-11-15 22:00:25 +0800

    rocketmq filter demo

---
+++ demo-recoketmq/src/main/java/com/code/example/messageselector/Producer.java
@@ -0,0 +1,36 @@
+package com.code.example.messageselector;
+
+import com.code.example.simple.RocketMqUtil;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Producer {
+    public static void main(String[] args) throws Exception {
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = RocketMqUtil.getDefaultMQProducer("select_producer_group");
+        producer.start();
+        List<String> tagList = Arrays.asList("TagA", "TagB", "TagC", "TagD");
+        for (int i = 0; i < 8; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            //创建消息实例 确定topic tag message
+            // Topic Tag Message body
+            int index = i % tagList.size();
+            String tagStr = tagList.get(index);
+            System.out.println("tagStr = " + tagStr);
+
+            Message msg = new Message("select_topic",
+                    tagStr, ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET)
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg, 10000);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
+        producer.shutdown();
+    }
+}
