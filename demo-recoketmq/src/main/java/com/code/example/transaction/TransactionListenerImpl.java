8411ba22d294c253c149e48d0ae78f9beb5df611
caicy <caicy@dxy.cn>
2019-11-18 21:46:31 +0800

    rocket mq 事务消息demo

--- demo-recoketmq/src/main/java/com/code/example/transaction/TransactionListenerImpl.java
+++ demo-recoketmq/src/main/java/com/code/example/transaction/TransactionListenerImpl.java
@@ -38,7 +38,7 @@ public class TransactionListenerImpl implements TransactionListener {
         //模拟执行本地事务end========
         //TODO 实际开发下面不需要我们手动返回，而是根据本地事务执行结果自动返回
         //1、二次确认消息，然后消费者可以消费
-        StaticLog.info("本地事务执行返回{}",result);
+        StaticLog.info("本地事务执行返回{}", result);
         if (result == 0) {
             return LocalTransactionState.COMMIT_MESSAGE;
         }
@@ -58,8 +58,8 @@ public class TransactionListenerImpl implements TransactionListener {
      * <p>
      * executeLocalTransaction（） 提交的二次确认最终未到达 MQ Server，经过固定时间后 MQ Server 将对该消息发起消息回查。
      * 或者 executeLocalTransaction（） 返回LocalTransactionState.UNKNOW   也会调用回查接口
-     *
-     *
+     * <p>
+     * <p>
      * 如果checkLocalTransaction() 继续返回unkonw (或者超时不响应) mq server 会继续调用回查
      */
     @Override
@@ -68,13 +68,16 @@ public class TransactionListenerImpl implements TransactionListener {
 //        String key = msg.getKeys();
         JSONObject jsonObject = localTrans.get(msg.getTransactionId());
         Integer status = jsonObject.getInteger("status");
-        //TODO 1、必须根据key先去检查本地事务消息是否完成。
+        //  根据msg信息检查本地事务消息是否完成。 3种可能
+        //  1.依然无法确定本地事务的执行结果，返回unknow，下次会继续回查结果
+        //  2.查到本地事务执行成功，返回COMMIT_MESSAGE，producer继续发送确认消息（此逻辑无需自己写，mq本身提供）
+        //  3.查询到本地事务执行失败，需要回滚消息。
         /**
          * 因为有种情况就是：上面本地事务执行成功了，但是return LocalTransactionState.COMMIT_MESSAG的时候
          * 服务挂了，那么最终 Brock还未收到消息的二次确定，还是个半消息 ，所以当重新启动的时候还是回调这个回调接口。
          * 如果不先查询上面本地事务的执行情况 直接在执行本地事务，那么就相当于成功执行了两次本地事务了。
          */
-        // TODO 2、这里返回要么commit 要么rollback。没有必要在返回 UNKNOW
+
         return LocalTransactionState.COMMIT_MESSAGE;
     }
 
