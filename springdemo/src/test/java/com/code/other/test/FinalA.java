b7b844128733a355651e0c7cf3a1ceaf699a39c3
caicy <caicy@dxy.cn>
2020-01-31 09:56:20 +0800

    继承知识点测试

---
+++ springdemo/src/test/java/com/code/other/test/FinalA.java
@@ -0,0 +1,14 @@
+package com.code.other.test;
+
+/**
+ * @author ccy
+ * @description
+ * @time 2020-01-30 20:13
+ */
+public class FinalA {
+    public final void get(){
+    }
+
+    public void getB(){
+    }
+}
---
+++ springdemo/src/test/java/com/code/other/test/FinalB.java
@@ -0,0 +1,18 @@
+package com.code.other.test;
+
+/**
+ * @author ccy
+ * @description
+ * @time 2020-01-30 20:14
+ */
+public class FinalB extends FinalA{
+    public void get(int x){
+        Integer a=null;
+        a=a+1;
+    }
+
+    @Override
+    public void getB() {
+        super.getB();
+    }
+}
