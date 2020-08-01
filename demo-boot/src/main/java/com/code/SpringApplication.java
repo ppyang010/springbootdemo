90c181feafd2513b1d7249e3d2c6c4b393cc15de
caicy <caicy@dxy.cn>
2020-08-01 22:57:09 +0800

    初始化

---
+++ demo-boot/src/main/java/com/code/SpringApplication.java
@@ -0,0 +1,20 @@
+package com.code;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author ccy
+ * @description
+ * @time 2020-07-31 16:53
+ */
+@SpringBootApplication
+public class SpringApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication springApplication = new org.springframework.boot.SpringApplication(SpringApplication.class);
+        //设置为 非 web 应用
+        //springApplication.setWebEnvironment(false);
+        ConfigurableApplicationContext context = springApplication.run(args);
+    }
+}
