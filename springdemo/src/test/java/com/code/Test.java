5ae43b5d70852173fd89802427a5da8fe1ae03fa
CCY <644724460@qq.com>
2020-08-14 09:00:16 +0800

    demo

--- springdemo/src/test/java/com/code/Test.java
+++ springdemo/src/test/java/com/code/Test.java
@@ -1,6 +1,7 @@
 package com.code;
 
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Phaser;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
@@ -8,11 +9,14 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Test {
     public static void main(String[] args) {
-        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
-        Lock readLock = readWriteLock.readLock();
-        Lock writeLock = readWriteLock.writeLock();
-        Condition condition = writeLock.newCondition();
-        condition.signal();
+//        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+//        Lock readLock = readWriteLock.readLock();
+//        Lock writeLock = readWriteLock.writeLock();
+//        Condition condition = writeLock.newCondition();
+//        condition.signal();
 //        new CyclicBarrier();
+        Phaser phaser = new Phaser(10);
+//        phaser
+
     }
 }
