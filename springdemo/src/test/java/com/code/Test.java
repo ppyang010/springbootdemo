afd563ae4a080cd44470bb5cc4714fede10f1d46
CCY <644724460@qq.com>
2020-08-14 08:31:02 +0800

    demo

---
+++ springdemo/src/test/java/com/code/Test.java
@@ -0,0 +1,18 @@
+package com.code;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class Test {
+    public static void main(String[] args) {
+        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        Lock readLock = readWriteLock.readLock();
+        Lock writeLock = readWriteLock.writeLock();
+        Condition condition = writeLock.newCondition();
+        condition.signal();
+//        new CyclicBarrier();
+    }
+}
