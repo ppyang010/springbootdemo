69a34df92f473d43859a6254b04c4e6d14f9b9f8
caicy <caicy@dxy.cn>
2020-12-16 17:31:22 +0800

    2020年12月16日17:31:16

--- springdemo/src/test/java/com/code/MockRandom.java
+++ springdemo/src/test/java/com/code/MockRandom.java
@@ -14,23 +14,29 @@ public class MockRandom {
 //        for (int i = 0; i < 10; i++) {
 //            random(x);
 //        }
-//        BigDecimal base = new BigDecimal("0.95");
-//        for (int i = 0; i < 10; i++) {
-//           base = base.multiply(new BigDecimal("0.95"));
-//        }
-//        System.out.println(base);
-        cal();
+        BigDecimal base = new BigDecimal("0.95");
+        for (int i = 0; i < 10; i++) {
+           base = base.multiply(new BigDecimal("0.95"));
+        }
+        System.out.println(base);
+//        cal();
     }
 
     public static void cal() {
-        BigDecimal base = new BigDecimal("0.9427");//东
-        base = base.multiply(new BigDecimal("0.9975"));//和光
-        base = base.multiply(new BigDecimal("0.9217"));//春和
-        base = base.multiply(new BigDecimal("0.867"));//联发现房
-        base = base.multiply(new BigDecimal("0.8921"));//桃源滨江
-        base = base.multiply(new BigDecimal("0.515"));//联发北秀
-        base = base.multiply(new BigDecimal("0.9531"));//荣望
-        base = base.multiply(new BigDecimal("0.906"));//展望预估
+        BigDecimal base = new BigDecimal("0.9427");//东 1
+        base = base.multiply(new BigDecimal("0.9975"));//和光 2
+        base = base.multiply(new BigDecimal("0.9217"));//春和 3
+        base = base.multiply(new BigDecimal("0.867"));//联发现房 4
+        base = base.multiply(new BigDecimal("0.8921"));//滨江沁语 桃源 5
+        base = base.multiply(new BigDecimal("0.515"));//联发北秀 6
+        base = base.multiply(new BigDecimal("0.9531"));//荣望 7
+        base = base.multiply(new BigDecimal("0.906"));//展望 8
+        base = base.multiply(new BigDecimal("0.7946"));//保利潮起云上府  20.54% 9
+        base = base.multiply(new BigDecimal("0.91"));//大家天筑  9% 预计 no10
+        base = base.multiply(new BigDecimal("0.9"));//滨江沁语 桃源  10% 预计
+
+
+
 
         System.out.println(base);
     }
