1d5bf355bf31f05cac2ef39f089fe11c7fb9e247
CCY <644724460@qq.com>
2020-12-22 22:21:52 +0800

    2020年12月22日 22:21:44

--- springdemo/src/test/java/com/code/MockRandom.java
+++ springdemo/src/test/java/com/code/MockRandom.java
@@ -10,40 +10,43 @@ import java.util.concurrent.ThreadLocalRandom;
  */
 public class MockRandom {
     public static void main(String[] args) {
-//        int x = 90;
-//        for (int i = 0; i < 10; i++) {
-//            random(x);
-//        }
-        BigDecimal base = new BigDecimal("0.95");
-        for (int i = 0; i < 10; i++) {
-           base = base.multiply(new BigDecimal("0.95"));
+        int x = 90;
+        int times = 0;
+        for (int i = 0; i < 10000; i++) {
+            if (random(x)) {
+                times++;
+            }
         }
-        System.out.println(base);
+        System.out.println("times=" + times);
+//        BigDecimal base = new BigDecimal("0.95");
+//        for (int i = 0; i < 10; i++) {
+//            base = base.multiply(new BigDecimal("0.95"));
+//        }
+//        System.out.println(base);
 //        cal();
     }
 
     public static void cal() {
-        BigDecimal base = new BigDecimal("0.9427");//东 1
-        base = base.multiply(new BigDecimal("0.9975"));//和光 2
-        base = base.multiply(new BigDecimal("0.9217"));//春和 3
-        base = base.multiply(new BigDecimal("0.867"));//联发现房 4
-        base = base.multiply(new BigDecimal("0.8921"));//滨江沁语 桃源 5
-        base = base.multiply(new BigDecimal("0.515"));//联发北秀 6
-        base = base.multiply(new BigDecimal("0.9531"));//荣望 7
-        base = base.multiply(new BigDecimal("0.906"));//展望 8
-        base = base.multiply(new BigDecimal("0.7946"));//保利潮起云上府  20.54% 9
-        base = base.multiply(new BigDecimal("0.91"));//大家天筑  9% 预计 no10
-        base = base.multiply(new BigDecimal("0.9"));//滨江沁语 桃源  10% 预计
-
-
+        BigDecimal base = new BigDecimal("0.9427");//1东
+        base = base.multiply(new BigDecimal("0.9975"));//2 和光
+        base = base.multiply(new BigDecimal("0.9217"));//3 春和
+        base = base.multiply(new BigDecimal("0.867"));//4 联发现房
+        base = base.multiply(new BigDecimal("0.8921"));//5 滨江沁语 桃源
+        base = base.multiply(new BigDecimal("0.515")); //6 联发北秀
+        base = base.multiply(new BigDecimal("0.9531"));//7 荣望轩
+        base = base.multiply(new BigDecimal("0.906")); //8  融信展望
+        base = base.multiply(new BigDecimal("0.7946"));//9 保利潮起云上府  中签率20.54%
+        base = base.multiply(new BigDecimal("0.935")); //10  大家天筑  中签率6.5%
+        base = base.multiply(new BigDecimal("0.9"));  //11 滨江沁语 桃源板块   预计中签率10%
 
 
         System.out.println(base);
     }
 
-    public static void random(int x) {
+    public static boolean random(int x) {
         int r = ThreadLocalRandom.current().nextInt(1000);
         System.out.println("r=" + r);
         System.out.println(r <= x);
+        return r <= x;
     }
 }
