874dc29693b8c8f262dff19903e2f2e4eba98c97
CCY <644724460@qq.com>
2021-05-24 01:54:06 +0800

    2021年5月24日 01:53:58

--- springdemo/src/test/java/com/code/MyClassLoader.java
+++ springdemo/src/test/java/com/code/MyClassLoader.java
@@ -5,9 +5,6 @@ import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.security.CodeSource;
-import java.security.ProtectionDomain;
-import java.security.cert.Certificate;
 
 /**
  * @author ccy
@@ -19,15 +16,21 @@ public class MyClassLoader extends ClassLoader {
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
-//        Semaphore one = new Semaphore(0);
-//        Semaphore two = new Semaphore(0);
-//        two.release();
-//        two.acquire();
-//        CyclicBarrier cyclicBarrier = new CyclicBarrier(2);
-//        CountDownLatch countDownLatch = new CountDownLatch(10);
-//        cyclicBarrier.await()
-//        HashSet<Object> objects = new HashSet<>();
-        return super.loadClass(name);
+        synchronized (getClassLoadingLock(name)) {
+            // First, check if the class has already been loaded
+            Class<?> c = findLoadedClass(name);
+            if (c == null) {
+                if(name.startsWith("xxx")){
+                    //满足自定义的条件 直接去加载类 不走双亲委派机制 打破双亲委派机制
+                    c = this.findClass(name);
+                }else {
+                    //不满足自定义的条件,继续走双亲委派机制  例如 java.lang.* 下面的类 沙箱安全机制无法自定义
+                    c = super.loadClass(name);
+                }
+            }
+
+            return c;
+        }
     }
 
     @Override
@@ -49,8 +52,6 @@ public class MyClassLoader extends ClassLoader {
         }
         return super.findClass(name);
     }
-
-
 
 
 //    private void myDefineClass(byte[] b, int off, int len) {
