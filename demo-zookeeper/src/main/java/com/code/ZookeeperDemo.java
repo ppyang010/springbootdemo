f903e668ed2f80425950491ad3ee5f0c9124fbea
caicy <caicy@dxy.cn>
2021-06-19 16:08:09 +0800

    2021年06月19日16:07:52

--- demo-zookeeper/src/main/java/com/code/ZookeeperDemo.java
+++ demo-zookeeper/src/main/java/com/code/ZookeeperDemo.java
@@ -10,6 +10,7 @@ import org.apache.zookeeper.ZooDefs;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author ccy
@@ -19,7 +20,9 @@ import java.util.List;
 public class ZookeeperDemo {
 
     public static void main(String[] args) throws Exception {
-        CuratorFramework client = CuratorFrameworkFactory.newClient("192.168.190.103:2181", new RetryNTimes(10, 5000));
+//        String adr = "192.168.190.103:2181";
+        String adr = "127.0.0.1:2181";
+        CuratorFramework client = CuratorFrameworkFactory.newClient(adr, new RetryNTimes(10, 5000));
         client.start();// 连接
         // 获取子节点，顺便监控子节点
         List<String> children = client.getChildren().usingWatcher(new CuratorWatcher() {
@@ -30,19 +33,36 @@ public class ZookeeperDemo {
         }).forPath("/");
         System.out.println(children);
         // 创建节点
-        String path = "/test_ccy123";
-        String result = client.create().withMode(CreateMode.PERSISTENT).withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE).forPath(path, "Data".getBytes());
+        String path = "/test_ccy123123";
+        String result = client
+                //创建节点或设置值
+                .create().orSetData()
+                .withProtection()
+                //节点类型 永久
+                .withMode(CreateMode.PERSISTENT)
+                //权限,全部
+                .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
+
+                .forPath(path, "Data123".getBytes());
         System.out.println(result);
         // 设置节点数据
         client.setData().forPath(path, "111".getBytes());
         client.setData().forPath(path, "222".getBytes());
         byte[] pathData = client.getData().forPath(path);
         System.out.println(new String(pathData, StandardCharsets.UTF_8));
+
         // 删除节点
 //        System.out.println(client.checkExists().forPath("/test"));
 //        client.delete().withVersion(-1).forPath("/test");
 //        System.out.println(client.checkExists().forPath("/test"));
 //        client.close();
 //        System.out.println("OK！");
+
+        Scanner s = new Scanner(System.in);
+
+        while(true){
+            String line = s.nextLine();
+            System.out.println(line);
+        }
     }
 }
